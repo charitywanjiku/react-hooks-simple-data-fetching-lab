3781a787e4738dfec7569f235bae04a8523140ae
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-02-05 23:21:57 +0300

    INTERESTING

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React , {useEffect, useState} from "react";
+function App (){
+    const [Img , setImg] = useState(null)
+   useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => setImg(data.message));
+  },[]); 
+    return(
+        <div>
+      {Img ? (
+        <img src={Img} alt="A Random Dog" />
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+    )
+
+};
+export default App;
